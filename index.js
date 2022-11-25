5cae9a399ea5020a7dfea59cb58dffc59c6e559f
youssef-attai <youssefgalalnazem@gmail.com>
2022-11-26 00:19:35 +0200

    Add markAsDone

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 class Task {
+  static latestId = 0;
+
   constructor(title, description) {
+    this.taskId = ++latestId;
     this.taskTitle = title;
     this.taskDescription = description;
-    this.done = false;
   }
 }
 
@@ -13,6 +15,10 @@ class ToDoList {
 
   addTask = (task) => {
     this.tasks.push(task);
+  };
+
+  markTaskDone = (targetId) => {
+    this.tasks = this.tasks.filter((task) => task.taskId !== targetId);
   };
 
   getTasks = () => {
